3ddb168b22d6068d2892d8a0a7fadf76afd67c30
Felicity Brammer <felicitybrammer@gmail.com>
2022-01-17 18:18:49 -0700

    refactor category menu component

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,6 +3,8 @@ import {
     UPDATE_CATEGORIES,
     UPDATE_CURRENT_CATEGORY
   } from "./actions";
+import { useReducer } from 'react';
+
   
   export const reducer = (state, action) => {
     switch (action.type) {
@@ -32,3 +34,7 @@ import {
         return state;
     }
   };
+
+  export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+  }
